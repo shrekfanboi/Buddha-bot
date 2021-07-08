cfe0161f8e768f408abc6d17f6579082db5bfe12
Mainak Mondal <81791300+shrekfanboi@users.noreply.github.com>
2021-07-08 12:54:31 +0530

    Update img_search.js

--- commands/fun/img_search.js
+++ commands/fun/img_search.js
@@ -1,6 +1,7 @@
 module.exports = {
     name:'img_search',
     args:true,
+    cooldown:5,
     execute(message,args){
         require('./modules/images').GetRandomImage(message,args.join(' '));
     }
