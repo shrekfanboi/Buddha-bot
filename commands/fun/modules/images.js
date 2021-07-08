bb3fa25808055bcef34632193aefe9a868c0a5b0
Mainak Mondal <81791300+shrekfanboi@users.noreply.github.com>
2021-07-08 12:28:50 +0530

    Add files via upload

---
+++ commands/fun/modules/images.js
@@ -0,0 +1,20 @@
+module.exports = {
+    type:"module",
+    async GetRandomImage(message,query){
+        const https = require('https');
+        const fs = require('fs');
+        const YOUR_ACCESS_KEY = "b2jDl2odWLNdlU7ebCteM80uTjnfIfQJsje2uvBseLM";
+        const url = `https://api.unsplash.com/photos/random/?client_id=${YOUR_ACCESS_KEY}&query=${query}&count=1`;
+        https.get(url,(resp) => {
+            let data = '';
+            
+            resp.on('data', (chunk) => {
+                    data += chunk;
+            })
+            resp.on('end', () => {
+                data = JSON.parse(data)[0]['urls']['raw'];
+                message.channel.send(data);
+            })
+        })
+    }
+}
