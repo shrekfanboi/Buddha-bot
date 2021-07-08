c3f87a2a80d011c43c0236e98b47604d4976d8e1
Mainak Mondal <81791300+shrekfanboi@users.noreply.github.com>
2021-07-08 15:02:39 +0530

    Update ping.js

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -2,6 +2,10 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(message, args) {
-		message.channel.send('Pong.');
-	},
+		message.channel.send('Loading..').then(async(msg) =>{
+			msg.delete();
+			message.channel.send(`Latency: ${msg.createdTimestamp - message.createdTimestamp}ms`);
+			message.channel.send(`API Latency: ${Math.round(message.client.ws.ping)}ms`);
+		});
+	}
 };
