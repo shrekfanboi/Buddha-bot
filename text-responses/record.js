fd0a08bca9fd99d9239d817eb29753bc073166c6
Mainak Mondal <81791300+shrekfanboi@users.noreply.github.com>
2021-07-09 15:17:57 +0530

    Update record.js

--- text-responses/record.js
+++ text-responses/record.js
@@ -2,7 +2,8 @@ module.exports = {
     type:'module',
     Record(guild,msg,reply){
         const fs = require('fs');
-        var obj = JSON.parse(fs.readFileSync(__dirname+"\\customMessage.json","utf-8"));
+        const path = require('path');
+        var obj = JSON.parse(fs.readFileSync(path.join(__dirname,'customMessage.json'),"utf-8"));
         if(!obj[guild]){
             obj[guild] = {};
         }
@@ -13,7 +14,7 @@ module.exports = {
     },
     Delete(guild,msg){
         const fs = require('fs');
-        var obj = JSON.parse(fs.readFileSync(__dirname+"\\customMessage.json","utf-8"));
+        var obj = JSON.parse(fs.readFileSync(path.join(__dirname,'customMessage.json'),"utf-8"));
         if(!obj[guild][msg]){
             console.log('No items to delete');
             return;
