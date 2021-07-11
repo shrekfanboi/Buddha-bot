3caf0b316f76a7f650fb63e0cd49e14eba4dc7d7
Mainak Mondal <81791300+shrekfanboi@users.noreply.github.com>
2021-07-11 13:20:26 +0530

    Add files via upload

---
+++ commands/fun/modules/translate.js
@@ -0,0 +1,35 @@
+module.exports = {
+    type:'module',
+    Translate(query,lang,message){
+        const http = require('https');
+        const api_key = " e814fb41416741b9dc34";
+        url = `https://api.mymemory.translated.net/get?q=${query}!&langpair=en|${lang}&de=hebeho6220@nhmty.com`
+
+        var request = http.request(url, function (res) {
+            var data = '';
+            var translations = []
+            res.on('data', function (chunk) {
+                data += chunk;
+            });
+            res.on('end', function () {
+                data = JSON.parse(data);
+                try{
+                    translations.push(data['responseData']['translatedText']);
+                    translations.push(data['matches'][0]['translation']);
+                    translations.push(data['matches'][1]['translation']);
+                    translations.push(data['matches'][2]['translation'])
+                }
+                catch(e){
+                    console.log(e);
+                }
+                if(translations.length>0) message.reply(require('./embed').TranslatedMessage(query,translations));
+            })
+        })
+        request.on('error', function (e) {
+            console.log("Error!")
+            console.log(e.message);
+        });
+        request.end();
+    }
+}
+
