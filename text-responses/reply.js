15e4684f46894ba04d8791210196e08bf119cd30
Mainak Mondal <81791300+shrekfanboi@users.noreply.github.com>
2021-07-12 16:40:54 +0530

    Update reply.js

--- text-responses/reply.js
+++ text-responses/reply.js
@@ -3,17 +3,10 @@ module.exports = {
         const fs = require('fs');
         const path = require('path');
         const readline = require('readline');
-        var obj = JSON.parse(fs.readFileSync(path.join(__dirname,'customMessage.json')));
         temp = message.content.toLowerCase();
-        if(obj[message.guild.id]){
-            if(obj[message.guild.id].hasOwnProperty(temp)){
-                message.lineReply(obj[message.guild.id][temp]);
-                return;
-            }
-        }
         
         const file = readline.createInterface({
-            input:fs.createReadStream(path.join(__dirname,'customMessage.json')),
+            input:fs.createReadStream(path.join(__dirname,'sad_words.txt')),
             output:process.stdout,
             terminal:false
         });
