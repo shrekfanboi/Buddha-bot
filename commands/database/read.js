176c26ce48e97be9aecf59b2168cab4641d4590d
Mainak Mondal <81791300+shrekfanboi@users.noreply.github.com>
2021-07-16 16:13:41 +0530

    Update read.js

--- commands/database/read.js
+++ commands/database/read.js
@@ -1,7 +1,8 @@
 const { Pool } = require("pg")
 module.exports = {
     async Read(data){
-        pool = await require('./conn').Connect();
+        const path = require('path');
+        pool = await require(path.join(__dirname,'conn.js')).Connect();
         query = "SELECT * FROM custom_messages WHERE server_name=$1 AND message=$2";
         res = await pool.query(query,data);
         return res.rows[0];
