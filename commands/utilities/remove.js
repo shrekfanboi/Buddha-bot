c31e57f097dbdffced7cff219d59f6870df39267
Mainak Mondal <81791300+shrekfanboi@users.noreply.github.com>
2021-07-16 15:26:53 +0530

    Update remove.js

--- commands/utilities/remove.js
+++ commands/utilities/remove.js
@@ -5,6 +5,7 @@ module.exports = {
     execute(messaage,args){
         msg = args.join(' ').trim();
         msg = msg.toLowerCase();
+        if(msg == '') return;
         console.log(msg);
         require('./../database/delete').Delete([messaage.guild.name,msg])
         .then(() => messaage.reply('Message Deleted!'));
