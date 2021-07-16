d783531d286430bff129b6381e62a9e73a5f9056
Mainak Mondal <81791300+shrekfanboi@users.noreply.github.com>
2021-07-16 15:19:16 +0530

    Delete commands/database directory

--- commands/database/read.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg")
-module.exports = {
-    async Read(data){
-        pool = await require('./conn').Connect("mydb");
-        query = "SELECT * FROM custom_messages WHERE server_name=$1 AND message=$2";
-        res = await pool.query(query,data);
-        return res.rows[0];
-    }
-}
